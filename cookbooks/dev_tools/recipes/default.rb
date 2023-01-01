65473758726dc6c79a6620df0fd59ec7bf7e6b40
Joel Mann <jmann.creston@gmail.com>
2023-01-01 15:50:54 -0800

    Slight typo

--- cookbooks/dev_tools/recipes/default.rb
+++ cookbooks/dev_tools/recipes/default.rb
@@ -8,8 +8,9 @@ package 'git'
 package 'python3'
 package 'tar'
 
-cron 'run_chef' do 
+cron 'run_chef' do
   minute '5'
   command 'chef-solo -c $SOLO_FOLDER/chef-devserver/solo.rb -j $SOLO_FOLDER/chef-devserver/node.json'
   mailto 'jmann.creston@gmail.com'
+  action :create
 end
