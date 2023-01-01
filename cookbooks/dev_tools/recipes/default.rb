414db67feb21ff146b15dc4730b3d8e87ee28047
Joel Mann <jmann.creston@gmail.com>
2023-01-01 15:50:08 -0800

    Testing a cron config

--- cookbooks/dev_tools/recipes/default.rb
+++ cookbooks/dev_tools/recipes/default.rb
@@ -6,3 +6,10 @@
 
 package 'git'
 package 'python3'
+package 'tar'
+
+cron 'run_chef' do 
+  minute '5'
+  command 'chef-solo -c $SOLO_FOLDER/chef-devserver/solo.rb -j $SOLO_FOLDER/chef-devserver/node.json'
+  mailto 'jmann.creston@gmail.com'
+end
